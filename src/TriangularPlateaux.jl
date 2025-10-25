4722ecba21119fba999cdc358e062851ce15786e
Hao-Phys <hzhangphys@gmail.com>
2025-10-25 00:52:36 -0400

    Fix import statement for solve_self_consistent_nlswt to use the correct function signature

--- src/TriangularPlateaux.jl
+++ src/TriangularPlateaux.jl
@@ -1,7 +1,7 @@
 module TriangularPlateaux
 
 using Sunny
-import Sunny: Vec3, SelfConsistentNLSWT, solve_self_consistent_nlswt, PerturbationTheory, calculate_real_space_cubic_vertices_dipole, calculate_real_space_quartic_vertices_dipole, excitations_scnlswt, excitations_nlsw, calculate_mean_field_values_lswt
+import Sunny: Vec3, SelfConsistentNLSWT, solve_self_consistent_nlswt!, PerturbationTheory, calculate_real_space_cubic_vertices_dipole, calculate_real_space_quartic_vertices_dipole, excitations_scnlswt, excitations_nlsw, calculate_mean_field_values_lswt
 
 include("Types.jl")
 export UUDPlateau, UUUDPlateau
