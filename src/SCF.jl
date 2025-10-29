89176a077c3277d371c205d286da5b91aa0adf28
Hao-Phys <hzhangphys@gmail.com>
2025-10-28 23:03:20 -0400

    Refactor SCF functions to improve mean field value calculations and streamline energy computations

--- src/SCF.jl
+++ src/SCF.jl
@@ -1,30 +1,50 @@
-function renormalized_single_magnon_energies_scf(state::AbstractTriangularPlateau, mean_field_values::Vector{ComplexF64}, h, result_path::String; hcubature_opts::NamedTuple=NamedTuple(;), nlsolve_opts::NamedTuple=NamedTuple(;))
+function renormalized_single_magnon_energies_scf(state::AbstractTriangularPlateau, mean_field_values::Vector{ComplexF64}, h, result_path::String)
     (; q_ordering, nbands) = state
-    ftol = get(nlsolve_opts, :ftol, 1e-8)
     swt, _ = swts(state, h)
     scnlswt = SelfConsistentNLSWT(swt)
+
+    update_mean_field_values!(scnlswt, mean_field_values)
     try
-       sol_ftol = solve_self_consistent_nlswt!(scnlswt; mean_field_values, hcubature_opts, nlsolve_opts)
-       if sol_ftol > ftol
-           @warn "SCF did not converge to desired tolerance ftol=$ftol, got sol_ftol=$sol_ftol. Returing to NaN energies"
-           return fill(NaN, nbands)
-       end
-       try
-            E, _ = excitations_scnlswt(scnlswt, q_ordering)
-            E_renormalized = E[1:nbands]
-            jldsave(joinpath(result_path, "scf_$(typeof(state))_J1_$(state.J₁)_J2_$(round(state.J₂, digits=4))_h_$(round(h, digits=4)).jld2"); E_renormalized=E_renormalized, mean_field_values=scnlswt.mean_field_values, h=h)
-            return E_renormalized
-       catch _
-            @warn "Failed to compute renormalized energies after SCF, skipping renormalized energies calculation"
-            return fill(NaN, nbands)
-       end
+        E, _ = excitations_scnlswt(scnlswt, q_ordering)
+        E_renormalized = E[1:nbands]
+        jldsave(joinpath(result_path, "scf_$(typeof(state))_J1_$(state.J₁)_J2_$(round(state.J₂, digits=4))_h_$(round(h, digits=4)).jld2"); E_renormalized=E_renormalized, h=h)
+        return E_renormalized
     catch _
-        @warn "Failed SCF, skipping renormalized energies calculation"
+        @warn "Unstable state encountered during renormalized energy calculation"
         return fill(NaN, nbands)
     end
 end
 
-function find_lb_scf(state::AbstractTriangularPlateau, Δh, result_path::String; E_tol::Float64=1e-4, max_iter::Int=50, hcubature_opts::NamedTuple=NamedTuple(;), nlsolve_opts::NamedTuple=NamedTuple(;))
+function calculate_mean_field_values_hc_scf(state::AbstractTriangularPlateau, result_path::String; hcubature_opts::NamedTuple=NamedTuple(;), nlsolve_opts::NamedTuple=NamedTuple(;))
+    (; J₁, J₂) = state
+
+    if typeof(state) == UUDPlateau
+        @assert J₂ ≤ 1/8 * J₁ "UUD Plateau state unstable for J₂/J₁ > 1/8"
+    elseif typeof(state) == UUUDPlateau
+        @assert J₂ ≥ 1/8 * J₁ "UUUD Plateau state unstable for J₂/J₁ < 1/8"
+    else
+        error("Unsupported state type: $(typeof(state))")
+    end
+    
+    isdir(result_path) || mkpath(result_path)
+
+    hc = calculate_hc(state)
+    _, swt_ref = swts(state, hc)
+
+    mean_field_values = calculate_mean_field_values_lswt(swt_ref; hcubature_opts...)
+    println("Calculating reference mean field values of swt_ref")
+    scnlswt = SelfConsistentNLSWT(swt_ref)
+    solve_self_consistent_nlswt!(scnlswt; mean_field_values, hcubature_opts, nlsolve_opts)
+    mean_field_values = copy(scnlswt.mean_field_values)
+    println("Reference mean field values calculated")
+
+    filename = joinpath(result_path, "mean_field_values_hc_$(typeof(state))_J1_$(round(state.J₁, digits=4))_J2_$(round(state.J₂, digits=4)).jld2")
+    jldsave(filename; mean_field_values=mean_field_values, hc=hc)
+
+    return mean_field_values
+end
+
+function find_lb_ub_scf(state::AbstractTriangularPlateau, Δh, result_path::String; E_tol::Float64=1e-4, max_iter::Int=50, hcubature_opts::NamedTuple=NamedTuple(;), nlsolve_opts::NamedTuple=NamedTuple(;))
     (; J₂, nbands) = state
     isdir(result_path) || mkpath(result_path)
 
@@ -35,29 +55,18 @@ function find_lb_scf(state::AbstractTriangularPlateau, Δh, result_path::String;
     h_lo = h_curr
     h_hi = hc
 
-    swt, swt_ref = swts(state, hc)
-    mean_field_values = calculate_mean_field_values_lswt(swt_ref; hcubature_opts...)
-    println("Calculating reference mean field values of swt_ref")
-    scnlswt = SelfConsistentNLSWT(swt)
-    try
-        solve_self_consistent_nlswt!(scnlswt; mean_field_values, hcubature_opts, nlsolve_opts)
-    catch _
-        @warn "Failed SCF for reference state, cannot proceed. The corresponding state cannot be stable"
-        return NaN, NaN
-    end
+    mean_field_values = calculate_mean_field_values_hc_scf(state, result_path; hcubature_opts=hcubature_opts, nlsolve_opts=nlsolve_opts)
 
-    mean_field_values0 = copy(scnlswt.mean_field_values)
-    println("Reference mean field values calculated")
-
-    E = renormalized_single_magnon_energies_scf(state, mean_field_values0, h_curr, result_path; hcubature_opts, nlsolve_opts)[nbands]
+    E = renormalized_single_magnon_energies_scf(state, mean_field_values, h_curr, result_path)[nbands]
     @assert isnan(E) "Use more aggressive guess for Δh"
     h_curr = (h_hi + h_lo) / 2
 
+    println("Finding lower bound field h_lb")
     while !find_lb && iter ≤ max_iter
         println("--------------------------------")
         @show J₂, iter, h_curr
-        E = renormalized_single_magnon_energies_scf(state, mean_field_values0, h_curr, result_path; hcubature_opts, nlsolve_opts)[nbands]
-        println("Lowest magnon energy E=$E")
+        E = renormalized_single_magnon_energies_scf(state, mean_field_values, h_curr, result_path)[nbands]
+        println("Current lowest magnon energy E=$E")
         println("--------------------------------")
         if isnan(E)
             h_lo = h_curr
@@ -70,14 +79,7 @@ function find_lb_scf(state::AbstractTriangularPlateau, Δh, result_path::String;
         end
         iter += 1
     end
-
-    return h_curr, E
-end
-
-function find_ub_scf(state::AbstractTriangularPlateau, Δh, result_path::String; E_tol::Float64=1e-4, max_iter::Int=50, hcubature_opts::NamedTuple=NamedTuple(;), nlsolve_opts::NamedTuple=NamedTuple(;))
-    (; J₁, J₂, nbands) = state
-
-    isdir(result_path) || mkpath(result_path)
+    h_lb, E_lb = h_curr, E
 
     iter = 1
     find_ub = false
@@ -86,29 +88,16 @@ function find_ub_scf(state::AbstractTriangularPlateau, Δh, result_path::String;
     h_lo = hc
     h_hi = h_curr
 
-    swt, swt_ref = swts(state, hc)
-    mean_field_values = calculate_mean_field_values_lswt(swt_ref; hcubature_opts...)
-    println("Calculating reference mean field values of swt_ref")
-
-    scnlswt = SelfConsistentNLSWT(swt)
-    try
-        solve_self_consistent_nlswt!(scnlswt; mean_field_values, hcubature_opts, nlsolve_opts)
-    catch _
-        @warn "Failed SCF for reference state, cannot proceed. The corresponding state cannot be stable"
-        return NaN, NaN
-    end
-
-    mean_field_values0 = copy(scnlswt.mean_field_values)
-    println("Reference mean field values calculated")
-    E = renormalized_single_magnon_energies_scf(state, mean_field_values0, h_curr, result_path; hcubature_opts, nlsolve_opts)[nbands]
+    E = renormalized_single_magnon_energies_scf(state, mean_field_values, h_curr, result_path)[nbands]
     @assert isnan(E) "Use more aggressive guess for Δh"
     h_curr = (h_hi + h_lo) / 2
 
+    println("Finding upper bound field h_ub")
     while !find_ub && iter ≤ max_iter
         println("--------------------------------")
         @show J₂, iter, h_curr
-        E = renormalized_single_magnon_energies_scf(state, mean_field_values0, h_curr, result_path; hcubature_opts, nlsolve_opts)[nbands]
-        println("Lowest magnon energy E=$E")
+        E = renormalized_single_magnon_energies_scf(state, mean_field_values, h_curr, result_path)[nbands]
+        println("Current lowest magnon energy E=$E")
         println("--------------------------------")
         if isnan(E)
             h_hi = h_curr
@@ -122,7 +111,80 @@ function find_ub_scf(state::AbstractTriangularPlateau, Δh, result_path::String;
         iter += 1
     end
 
-    return h_curr, E
+    h_ub, E_ub = h_curr, E
+
+    return h_lb, h_ub, E_lb, E_ub
+end
+
+function find_J2_bound_scf(state::AbstractTriangularPlateau, h, ΔJ2, result_path::String; E_tol::Float64=1e-4, max_iter::Int=50, hcubature_opts::NamedTuple=NamedTuple(;), nlsolve_opts::NamedTuple=NamedTuple(;))
+    (; J₁, J₂, nbands) = state
+    isdir(result_path) || mkpath(result_path)
+    @assert J₂ == 1/8 * J₁ "Initial J₂ must be at the classical boundary value of 1/8 J₁"
+
+    mean_field_values = calculate_mean_field_values_hc_scf(state, result_path; hcubature_opts=hcubature_opts, nlsolve_opts=nlsolve_opts)
+
+    iter = 1
+    find_bound = false
+
+    if typeof(state) == UUDPlateau
+        J2_lo = J₂
+        J2_hi = J₂ + ΔJ2
+        J2_curr = J2_hi
+        state_curr = UUDPlateau(J₁, J2_curr, :scf)
+        E = renormalized_single_magnon_energies_scf(state_curr, mean_field_values, h, result_path)[nbands]
+        @assert isnan(E) "Use more aggressive guess for ΔJ2, lowest magnon energy is $E"
+        J2_curr = (J2_lo + J2_hi) / 2
+        println("Finding J2 bound for UUD Plateau")
+        while !find_bound && iter ≤ max_iter
+            println("--------------------------------")
+            @show J2_curr, iter
+            state_curr = UUDPlateau(J₁, J2_curr, :scf)
+            E = renormalized_single_magnon_energies_scf(state_curr, mean_field_values, h, result_path)[nbands]
+            println("Current lowest magnon energy E=$E")
+            println("--------------------------------")
+            if isnan(E)
+                J2_hi = J2_curr
+                J2_curr = (J2_lo + J2_hi) / 2
+            elseif E > E_tol
+                J2_lo = J2_curr
+                J2_curr = (J2_lo + J2_hi) / 2
+            else
+                find_bound = true
+            end
+            iter += 1
+        end
+        return J2_curr, E
+    elseif typeof(state) == UUUDPlateau
+        J2_hi = J₂
+        J2_lo = J₂ - ΔJ2
+        J2_curr = J2_lo
+        state_curr = UUUDPlateau(J₁, J2_curr, :scf)
+        E = renormalized_single_magnon_energies_scf(state_curr, mean_field_values, h, result_path)[nbands]
+        @assert isnan(E) "Use more aggressive guess for ΔJ2, lowest magnon energy is $E"
+        J2_curr = (J2_lo + J2_hi) / 2
+        println("Finding J2 bound for UUUD Plateau")
+        while !find_bound && iter ≤ max_iter
+            println("--------------------------------")
+            @show J2_curr, iter
+            state_curr = UUUDPlateau(J₁, J2_curr, :scf)
+            E = renormalized_single_magnon_energies_scf(state_curr, mean_field_values, h, result_path)[nbands]
+            println("Current lowest magnon energy E=$E")
+            println("--------------------------------")
+            if isnan(E)
+                J2_lo = J2_curr
+                J2_curr = (J2_lo + J2_hi) / 2
+            elseif E > E_tol
+                J2_hi = J2_curr
+                J2_curr = (J2_lo + J2_hi) / 2
+            else
+                find_bound = true
+            end
+            iter += 1
+        end
+        return J2_curr, E
+    else
+        error("Unsupported state type: $(typeof(state))")
+    end
 end
 
 function magnetization_correction_scf(state::AbstractTriangularPlateau, h, mean_field_values; kwargs...)
--- src/TriangularPlateaux.jl
+++ src/TriangularPlateaux.jl
@@ -1,7 +1,7 @@
 module TriangularPlateaux
 
 using Sunny
-import Sunny: Vec3, SelfConsistentNLSWT, solve_self_consistent_nlswt!, PerturbationTheory, calculate_real_space_cubic_vertices_dipole, calculate_real_space_quartic_vertices_dipole, excitations_scnlswt, excitations_nlsw, calculate_mean_field_values_lswt, dynamical_matrix!, swt_hamiltonian_dipole_nlsw!, bogoliubov!, norm2
+import Sunny: Vec3, SelfConsistentNLSWT, solve_self_consistent_nlswt!, PerturbationTheory, calculate_real_space_cubic_vertices_dipole, calculate_real_space_quartic_vertices_dipole, excitations_scnlswt, excitations_nlsw, calculate_mean_field_values_lswt, dynamical_matrix!, swt_hamiltonian_dipole_nlsw!, bogoliubov!, norm2, update_mean_field_values!
 
 include("Types.jl")
 export UUDPlateau, UUUDPlateau
@@ -11,7 +11,7 @@ export swts
 
 using JLD2
 include("SCF.jl")
-export find_lb_scf, find_ub_scf, renormalized_single_magnon_energies_scf, magnetization_correction_scf
+export find_lb_ub_scf, find_J2_bound_scf, renormalized_single_magnon_energies_scf, magnetization_correction_scf
 
 import StaticArrays: SVector
 import HCubature: hcubature
